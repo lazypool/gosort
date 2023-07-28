441f49fb4b1607903a23c22371be184972a54b3a
lazypool <3416382833@qq.com>
2023-07-28 19:42:13 +0800

    insertionSort completed

--- sorts/insertionSort.go
+++ sorts/insertionSort.go
@@ -1,4 +1,12 @@
 package sorts
 
 func InsertionSort(arr []int) {
+	length := len(arr)
+	for i := 0; i < length; i++ {
+		for j := 0; j < i; j++ {
+			if arr[j] > arr[i] {
+				arr[i], arr[j] = arr[j], arr[i]
+			}
+		}
+	}
 }
