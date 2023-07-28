87eeb75384aee7b34d14ad278f88b6d33a108119
lazypool <3416382833@qq.com>
2023-07-28 19:36:37 +0800

    selectionSort completed

--- sorts/selectionSort.go
+++ sorts/selectionSort.go
@@ -1,4 +1,14 @@
 package sorts
 
 func SelectionSort(arr []int) {
+	length := len(arr)
+	for i := 0; i < length-1; i++ {
+		min := i
+		for j := i + 1; j < length; j++ {
+			if arr[min] > arr[j] {
+				min = j
+			}
+		}
+		arr[i], arr[min] = arr[min], arr[i]
+	}
 }
