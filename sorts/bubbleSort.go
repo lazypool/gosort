34f5a24a8c3053bfd76ce71bab2b9bb4cc2fbdb7
lazypool <3416382833@qq.com>
2023-07-28 19:35:49 +0800

    bubbleSort completed

--- sorts/bubbleSort.go
+++ sorts/bubbleSort.go
@@ -1,8 +1,9 @@
 package sorts
 
 func BubbleSort(arr []int) {
-	for i := 0; i < len(arr); i++ {
-		for j := 0; j < len(arr)-i-1; j++ {
+	length := len(arr)
+	for i := 0; i < length; i++ {
+		for j := 0; j < length-i-1; j++ {
 			if arr[j] > arr[j+1] {
 				arr[j], arr[j+1] = arr[j+1], arr[j]
 			}
