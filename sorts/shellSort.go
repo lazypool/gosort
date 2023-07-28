e56575ac986a250c009edfcca8b33357a3384292
lazypool <3416382833@qq.com>
2023-07-28 20:08:31 +0800

    shellSort completed

--- sorts/shellSort.go
+++ sorts/shellSort.go
@@ -1,4 +1,14 @@
 package sorts
 
 func ShellSort(arr []int) {
+	length := len(arr)
+	for gap := int(length / 2); gap > 0; gap /= 2 {
+		for i := gap; i < length; i++ {
+			for j := i; j >= gap; j -= gap {
+				if arr[j-gap] > arr[j] {
+					arr[j], arr[j-gap] = arr[j-gap], arr[j]
+				}
+			}
+		}
+	}
 }
