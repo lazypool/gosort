69f4ab65f1c695c7c849f18858f6e0965dcc1735
lazypool <3416382833@qq.com>
2023-07-30 01:20:17 +0800

    bucketSort completed

--- sorts/bucketSort.go
+++ sorts/bucketSort.go
@@ -1,4 +1,26 @@
 package sorts
 
 func BucketSort(arr []int) {
+	var min, max int
+	for i := 0; i < len(arr); i++ {
+		if arr[i] > max {
+			max = arr[i]
+		}
+		if arr[i] < min {
+			min = arr[i]
+		}
+	}
+	buckets := make([][]int, 10)
+	for _, v := range arr {
+		i := ((v - min) / (max - min) * 9)
+		buckets[i] = append(buckets[i], v)
+	}
+	j := 0
+	for _, bucket := range buckets {
+		if len(bucket) > 0 {
+			BubbleSort(bucket)
+			copy(arr[j:], bucket)
+			j += len(bucket)
+		}
+	}
 }
