761ab15b1fbc3efe685576a084eb953a1e1ce4cd
lazypool <3416382833@qq.com>
2023-07-29 19:00:36 +0800

    countingSort completed

--- sorts/countingSort.go
+++ sorts/countingSort.go
@@ -1,4 +1,27 @@
 package sorts
 
 func CountingSort(arr []int) {
+	length := countLen(arr)
+	counts := make([]int, length)
+	for _, num := range arr {
+		counts[num] += 1
+	}
+	sorted := 0
+	for i := 0; i < length; i++ {
+		for counts[i] > 0 {
+			arr[sorted] = i
+			sorted += 1
+			counts[i] -= 1
+		}
+	}
+}
+
+func countLen(arr []int) int {
+	var max int
+	for _, num := range arr {
+		if num > max {
+			max = num
+		}
+	}
+	return max + 1
 }
